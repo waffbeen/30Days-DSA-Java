52a45b748525feed0707c1f1c2ef0207a854b104
Prathvi Singh Rajput <139202775+waffbeen@users.noreply.github.com>
2025-05-12 22:15:02 +0530

    Update PalindromeNumber.java

--- Day01/PalindromeNumber.java
+++ Day01/PalindromeNumber.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public boolean isPalindrome(int x) {
+        int sum=0,temp=0,n;
+        n=x;
+        if(n<0)
+        return false;
+        else{
+        while(x!=0)
+        {
+            temp=x%10;
+            sum=(sum*10)+temp;
+            x=x/10;
+        }
+        if(sum==n)
+        return true;
+        else 
+        return false;
+    }
+    }
+}
