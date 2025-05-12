4fa3b5329883a9809e82ac2843e287f2b5342ab7
Prathvi Singh Rajput <139202775+waffbeen@users.noreply.github.com>
2025-05-12 22:01:49 +0530

    Update TwoSum.java

--- Day01/TwoSum.java
+++ Day01/TwoSum.java
@@ -1,1 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int sum=nums[0];
+        for(int i=0;i<(nums.length);i++)
+        {
+            for(int j=0;j<(nums.length);j++)
+            {
+                if(i==j)
+                continue;
+                else{
+                    if(nums[i]+nums[j]==target)
+                    return new int[]{i,j};
 
+                }
+            }
+        }
+        return new int[0];
+
+    }
+}
