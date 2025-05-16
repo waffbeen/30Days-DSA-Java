17a67896741ddb4a65c0dbf046943a4f9c394b93
Prathvi Singh Rajput <139202775+waffbeen@users.noreply.github.com>
2025-05-16 14:44:06 +0530

    Update Length of Last Word.java

--- Day04/Length of Last Word.java
+++ Day04/Length of Last Word.java
@@ -1,1 +1,12 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        
+        s = s.trim();
 
+        int lastSpaceIndex = s.lastIndexOf(' ');
+
+        return s.length() - lastSpaceIndex - 1;
+    }
+
+
+}
