b90b40eb67a4501e2dfde326ef24bc7035d9cc1c
Prathvi Singh Rajput <139202775+waffbeen@users.noreply.github.com>
2025-05-19 21:40:03 +0530

    Create 3Sum.java

---
+++ Day07/3Sum.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) left++; // Skip duplicates
+                    while (left < right && nums[right] == nums[right - 1]) right--; // Skip duplicates
+                    left++;
+                    right--;
+                } else if (sum < 0) left++;
+                else right--;
+            }
+        }
+        return res;
+    }
+}
